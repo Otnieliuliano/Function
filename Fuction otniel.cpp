26be36b49e5105f6756210ee45d4a2b773102ff4
Otniel Iuliano <56227989+Otnieliuliano@users.noreply.github.com>
2019-10-21 08:57:15 +0700

    Add files via upload

---
+++ Fuction otniel.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void LuasPersegiPanjang()
+{
+ int Panjang,Lebar;
+ cout << "Mengerjakan Luas Persegi Panjang \n";
+ cout << "Otniel Iuliano \n\n";
+ cout << "Masukkan Panjang: "; cin >> Panjang;
+ cout << "Masukkan Lebar: "; cin >> Lebar;
+ cout <<"Luas Persegi Panjang: " << Panjang*Lebar;
+}
+
+int main()
+{
+ LuasPersegiPanjang();
+ return 0;
+}
